c96a6d68346166846826176d8eb4cbb228a2669a
Lichenxing <lichenxing819@sgeo.cn>
2019-02-20 10:10:35 +0800

    添加高德地图的key

--- watermark/src/main/java/com/lcx/watermark/AMapGeocode.kt
+++ watermark/src/main/java/com/lcx/watermark/AMapGeocode.kt
@@ -21,37 +21,38 @@ object AMapGeocode {
      * @param lng 经度
      */
 
-     fun getRegeocode(context: Context, lat: Double, lng: Double) : String{
+     fun getRegeocode(context: Context, lat: Double, lng: Double) : String?{
         var addr = ""
         val geocodeSearch = GeocodeSearch(context)
         val latLonPoint = LatLonPoint(lat, lng)
         val query = RegeocodeQuery(latLonPoint, 10f, GeocodeSearch.GPS)
-        geocodeSearch.getFromLocationAsyn(query)
-        geocodeSearch.setOnGeocodeSearchListener(object : GeocodeSearch.OnGeocodeSearchListener {
-            /**
-             * 逆地理编码回调
-             */
-            override fun onRegeocodeSearched(regeocodeResult: RegeocodeResult?, i: Int) {
-                if (i == AMapException.CODE_AMAP_SUCCESS) {
-                    if (regeocodeResult != null && regeocodeResult.regeocodeAddress != null
-                        && regeocodeResult.regeocodeAddress.formatAddress != null
-                    ) {
-                        addr = regeocodeResult.regeocodeAddress.formatAddress
-                    } else {
-                        addr = ""
-                    }
-                } else {
-                    addr = ""
-                }
-            }
-
-            /**
-             * 地理编码回调
-             * @param geocodeResult
-             * @param i
-             */
-            override fun onGeocodeSearched(geocodeResult: GeocodeResult, i: Int) {}
-        })
-        return addr
+        val regeocodeAddress = geocodeSearch.getFromLocation(query)
+        return regeocodeAddress.formatAddress
+//        geocodeSearch.getFromLocationAsyn(query)
+//        geocodeSearch.setOnGeocodeSearchListener(object : GeocodeSearch.OnGeocodeSearchListener {
+//            /**
+//             * 逆地理编码回调
+//             */
+//            override fun onRegeocodeSearched(regeocodeResult: RegeocodeResult?, i: Int) {
+//                if (i == AMapException.CODE_AMAP_SUCCESS) {
+//                    if (regeocodeResult != null && regeocodeResult.regeocodeAddress != null
+//                        && regeocodeResult.regeocodeAddress.formatAddress != null
+//                    ) {
+//                        addr = regeocodeResult.regeocodeAddress.formatAddress
+//                    } else {
+//                        addr = ""
+//                    }
+//                } else {
+//                    addr = ""
+//                }
+//            }
+//
+//            /**
+//             * 地理编码回调
+//             * @param geocodeResult
+//             * @param i
+//             */
+//            override fun onGeocodeSearched(geocodeResult: GeocodeResult, i: Int) {}
+//        })
     }
 }
